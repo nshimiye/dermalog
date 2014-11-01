c299cabe860863b5fd28379373625ebb1a5d4e2d
Danilo Faria <df2553@columbia.edu>
2014-11-01 15:37:11 -0400

    authorization working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,27 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
- 
+
+  before_filter :authorize
+
+  delegate :allow?, to: :current_permission
+  helper_method :allow?
+
   private
 
   def current_user
     @current_user ||= Doctor.find(session[:user_id]) if session[:user_id]
   end
   helper_method :current_user
+
+  def current_permission
+    @current_permission ||= Permission.new(current_user)
+  end
+
+  def authorize
+    if !current_permission.allow?(params[:controller], params[:action])
+      redirect_to root_url, alert: "Not authorized."
+    end
+  end
 
 end
