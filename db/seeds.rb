fcc7ff2f52ac5c2694971f51e92816b937d53f2e
Danilo Faria <df2553@columbia.edu>
2014-11-08 19:25:13 -0500

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Doctor.create([{ name: 'SuperDoctor', state: 'AC', email: 'sd@g.com', password: '1234', password_confirmation: '1234', license:'1234', admin: true }])
+Doctor.create([{ name: 'Jack', state: 'PB', password: '1111', email: 'jack@g.com', password_confirmation: '1111', license:'1111', admin: false }])
