f62546fb7ac82c76a7616d458cbb8aca8ed02e49
Danilo Faria <df2553@columbia.edu>
2014-11-23 17:29:52 -0500

    a few more controller tests

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -2,7 +2,7 @@ class Doctor < ActiveRecord::Base
 	has_many :case_logs
 	has_many :images, through: :case_logs
 
-	# validates :email, :presence => true, :uniqueness => true, :format => {:with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+	validates :email, :presence => true, :uniqueness => true, :format => {:with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
 	attr_accessor :password_confirmation  
 	validates :password, confirmation: true
 	validates_presence_of :name, :state, :password, :license
