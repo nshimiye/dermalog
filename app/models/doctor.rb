be02749de6babc3a351dd16a1061bbf90fbc88da
Danilo Faria <df2553@columbia.edu>
2014-12-12 19:58:19 -0500

    Fixed uniqueness of license

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -6,5 +6,6 @@ class Doctor < ActiveRecord::Base
 	validates :email, :presence => true, :uniqueness => true, :format => {:with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
 	attr_accessor :password_confirmation  
 	validates :password, confirmation: true
-	validates_presence_of :name, :state, :password, :license
+	validates :license, :presence => true, :uniqueness => true
+	validates_presence_of :name, :state, :password
 end
