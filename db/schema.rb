ab3c2a946ce2724875c8e2f75f84a8fe2da22aeb
Marcellin Nshimiyimana <mn2587@colubia.edu>
2014-12-17 23:56:45 -0500

    fixed minor error

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20141212214959) do
 
   create_table "case_logs", force: true do |t|
